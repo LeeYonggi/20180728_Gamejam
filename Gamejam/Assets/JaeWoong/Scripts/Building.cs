f72974c199a0e8d8b3091277d4aec5282618407b
JaeWoong <woorin0101@gmail.com>
2018-07-28 19:52:50 +0900

    노트패드 완료

---
+++ Gamejam/Assets/JaeWoong/Scripts/Building.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Building : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    public int maxPatternNum;
+    public int[] notePattern;
+
+    public float gravityScale = -10.0f;
+    
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.useGravity = false;
+	}
+
+    private void Update()
+    {
+        
+    }
+
+    void FixedUpdate ()
+    {
+        rb.AddForce(new Vector3(0, gravityScale, 0), ForceMode.Acceleration);
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+
+}
---
+++ Gamejam/Assets/JaeWoong/Scripts/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //public Dictionary<string, List<int>> notePattern = new Dictionary<string, List<int>>();
+
+    public static GameManager instance;
+    public GameObject building;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        StartCoroutine("MakeBuilding");
+    }
+    
+    void Update()
+    {
+
+    }
+
+    IEnumerator MakeBuilding()
+    {
+        GameObject g = Instantiate<GameObject>(building);
+        Building b = g.GetComponent<Building>();
+
+        b.gravityScale = -10f;
+        b.maxPatternNum = 4;
+
+        b.notePattern = new int[b.maxPatternNum];
+        b.notePattern[0] = 1;
+        b.notePattern[1] = 2;
+        b.notePattern[2] = 3;
+        b.notePattern[3] = 4;
+        
+        PlayerNotePad.instance.buildingInfo = b;
+        yield return null;
+        
+    }
+}
